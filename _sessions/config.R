a4a1ed143f2dde23aeff3ed33bc16a67d3c50c02
Michael Schulte <michael@MacBook-Disaster-2.fritz.box>
2022-09-14 15:18:55 +0200

    data fixed

--- _sessions/config.R
+++ _sessions/config.R
@@ -4,5 +4,5 @@ base_url = 'https://therbootcamp.github.io/Intro2R_Unibe_2022/'
 raw_url = 'https://raw.githubusercontent.com/therbootcamp/Intro2R_Unibe_2022/'
 
 # if TRUE = show solutions in practicals
-value_echo = FALSE
-value_eval = FALSE
+value_echo = TRUE
+value_eval = TRUE
