6afbd871bbc43fd666c6ca1984dded2fe61c622d
Michael Schulte <michael@MacBook-Disaster-2.fritz.box>
2022-09-14 15:02:55 +0200

    rerender Rmd files for results in practicals

--- _sessions/config.R
+++ _sessions/config.R
@@ -3,6 +3,6 @@ session_url = 'https://therbootcamp.github.io/Intro2R_Unibe_2022/_sessions/'
 base_url = 'https://therbootcamp.github.io/Intro2R_Unibe_2022/'
 raw_url = 'https://raw.githubusercontent.com/therbootcamp/Intro2R_Unibe_2022/'
 
-# show solutions in practicals
+# if TRUE = show solutions in practicals
 value_echo = FALSE
 value_eval = FALSE
